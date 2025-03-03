5b48c8003efa301dd9b84ecb0eb89f376d00ef1e
Vejmelek Viktoria <viktoria.vejmelek@gmx.at>
2025-03-03 16:50:39 +0100

    Implement login and registration functionality with corresponding UI updates

---
+++ project/htdocs/assets/js/login.js
@@ -0,0 +1,9 @@
+function getLogin() {
+    document.getElementById('main_register').style.display = 'none';
+    document.getElementById('main_login').style.display = 'grid';
+}
+
+function getRegister() {
+    document.getElementById('main_register').style.display = 'grid';
+    document.getElementById('main_login').style.display = 'none';
+}
