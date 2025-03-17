1085f4894db7f486cb9c1724d02ff9abf7df0a84
annar2703 <anna.reder2703ar@gmail.com>
2025-03-17 10:29:54 +0100

    Benutzername ändern

--- project/htdocs/assets/js/settings.js
+++ project/htdocs/assets/js/settings.js
@@ -1,30 +1,22 @@
 function showEditProfil() {
     document.getElementById("outp").innerHTML = `
           <div id="profilBearbeiten">
-              <h2>Profil bearbeiten</h2>
-              <p>Informationen, die du hier hinzufügst, sind für alle Nutzer sichtbar, die dein Profil ansehen können.</p>
+              <label style=" font-size: 30px;" for="profilbild">Profil bearbeiten</label> 
+              <p style=" font-size: 13px; color: grey;">Informationen, die du hier hinzufügst, sind für alle Nutzer sichtbar, die dein Profil ansehen können.</p>
               <div>
-                  <label for="profilbild">Profilbild</label> <br><br>
-                  <img src="../../assets/images/profilPicture_test.png" alt="Profilbild" id="profilbild"/>
-                  <button id="change_button" onclick="changeProfilePic()">Ändern</button>
+                  <label style=" font-size: 20px;" for="profilbild">Profilbild</label> <br><br>
+                  <img id="profilPic" src="../../assets/images/profilPicture_test.png" alt="Profilbild"/>
               </div>
               <div> 
                   <br> 
                   <br>
-                  <label for="benutzername">Benutzername</label>
-                  <p>@viktoria.explorer69</p>
-                  <button onclick="changeProfilePic()">Ändern</button>
+                  <label  style=" font-size: 20px;" for="benutzername">Benutzername</label>
+                  <p id="username">@viktoria.explorer69</p>
+                  <button class="buttons" onclick="changeUserName()">Ändern</button>
 
               </div>
           </div>
-          <style>
-              #profilbild {
-                  width: 100px;
-                  height: 100px;
-                  border-radius: 50%;
-                  object-fit: cover;
-              }
-          </style>
+          
       `;
   }
 
@@ -37,3 +29,19 @@ function showEditProfil() {
   
   showEditProfil();
   
+
+  function changeUserName() {
+    document.getElementById("usernameOverlay").style.display = "flex"; // Zeigt das Overlay an
+}
+
+function closeOverlay() {
+    document.getElementById("usernameOverlay").style.display = "none"; // Versteckt das Overlay
+}
+
+function saveUserName() {
+    let newUserName = document.getElementById("newUserName").value;
+    if (newUserName.trim() !== "") {
+        document.getElementById("username").textContent = "@" + newUserName;
+    }
+    closeOverlay(); // Overlay schließen nach dem Speichern
+}
