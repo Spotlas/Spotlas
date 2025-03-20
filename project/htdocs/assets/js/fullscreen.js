443b956e1325f6130f3f197b384e267a05ff7f97
Vejmelek Viktoria <viktoria.vejmelek@gmx.at>
2025-03-20 09:33:03 +0100

    Implement location details fetching and user profile API

--- project/htdocs/assets/js/fullscreen.js
+++ project/htdocs/assets/js/fullscreen.js
@@ -6,3 +6,22 @@ document.querySelectorAll('.rating input').forEach((radio) => {
         e.target.parentNode.classList.add('checked');
     });
 });
+
+// Holt detaillierte Informationen zu einer Location inkl. Kommentare und Bilder
+function fetchLocationDetails(id) {
+    fetch(`../../api/location.php?action=details&id=${id}`)
+        .then(response => response.text())
+        .then(data => {
+            console.log('Location details:', data);
+
+            // Update location details
+            //const location = data.location;
+            document.querySelector('#name_spot').innerHTML = data.location.name;
+
+        })
+        .catch(error => console.error('Error fetching location details:', error));
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchLocationDetails(2);
+});
