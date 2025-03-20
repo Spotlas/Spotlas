d9d5e8c6e80c29e674f7ad722353d5b0dfb9842e
Vejmelek Viktoria <viktoria.vejmelek@gmx.at>
2025-03-20 09:50:20 +0100

    Fetch location details as JSON and update rating display logic

--- project/htdocs/assets/js/fullscreen.js
+++ project/htdocs/assets/js/fullscreen.js
@@ -10,13 +10,23 @@ document.querySelectorAll('.rating input').forEach((radio) => {
 // Holt detaillierte Informationen zu einer Location inkl. Kommentare und Bilder
 function fetchLocationDetails(id) {
     fetch(`../../api/location.php?action=details&id=${id}`)
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
             console.log('Location details:', data);
 
+            const location = data.data.location;
+            const rating = data.data.average_rating;
+
             // Update location details
             //const location = data.location;
-            document.querySelector('#name_spot').innerHTML = data.location.name;
+            document.querySelector('#name_spot').innerHTML = location.name;
+            document.querySelectorAll('.rating input').forEach(input => {
+                if (parseInt(input.value) === average_rating) {
+                    input.checked = true;
+                }
+            });
+            document.querySelector('#description_spot').innerHTML = location.description;
+            document.querySelector('#name_spot').innerHTML = location.name;
 
         })
         .catch(error => console.error('Error fetching location details:', error));
