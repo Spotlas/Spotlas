6e152d9311ae9dd60d1540604dc6b2383d9cc4dd
Vejmelek Viktoria <viktoria.vejmelek@gmx.at>
2025-03-22 11:32:26 +0100

    Refactor rating display; replace radio buttons with star icons and update CSS for improved styling

--- project/htdocs/assets/js/fullscreen.js
+++ project/htdocs/assets/js/fullscreen.js
@@ -35,14 +35,11 @@ function fetchLocationDetails(id) {
             const location = data.data.location;
             const rating = data.data.average_rating;
 
+            generateStars(rating);
+
             // Update location details
             //const location = data.location;
             document.querySelector('#name_spot').innerHTML = location.name;
-            document.querySelectorAll('.rating input').forEach(input => {
-                if (parseInt(input.value) === rating) {
-                    input.checked = true;
-                }
-            });
             document.querySelector('#description_spot').innerHTML = location.description;
             document.querySelector('#name_category').innerHTML = location.name + ', ' + location.category;
             document.querySelector('#website_url').innerHTML = location.website_url;
@@ -58,9 +55,12 @@ function fetchLocationDetails(id) {
 }); */
 
 function rate(rating) {
-    const pointId = 2;
+    const pointId = 1;
     const userId = 3 // TODO: Get user ID from session
-    rateLocation(pointId, userId, rating);    
+    rateLocation(pointId, userId, rating);  
+    
+    // Update rating display
+    fetchLocationDetails(pointId);
 }
 
 function rateLocation(id, userId, rating) {
@@ -78,3 +78,36 @@ function rateLocation(id, userId, rating) {
         })
         .catch(error => console.error('Error rating location:', error));
   }
+
+  function generateStars(rating) {
+    let fullStars = Math.floor(rating); // Ganze Sterne
+    let halfStar = rating % 1 >= 0.5 ? 1 : 0; // Halber Stern falls >= 0.5
+    let emptyStars = 5 - fullStars - halfStar; // Leere Sterne berechnen
+    
+    let starHTML = "";
+
+    for (let i = 0; i < fullStars; i++) {
+        starHTML += '<i class="fa fa-star"></i>';
+    }
+    if (halfStar) {
+        starHTML += '<i class="fa fa-star-half-alt"></i>';
+    }
+    for (let i = 0; i < emptyStars; i++) {
+        starHTML += '<i class="fa fa-star" style="color: lightgray;"></i>';
+    }
+
+    document.getElementById("spot_rating").innerHTML = starHTML;
+}
+
+    let isSaved = false;
+    const bookmarkIcon = document.getElementById("bookmarkIcon");
+
+    bookmarkIcon.addEventListener("click", function () {
+        isSaved = !isSaved; // Umschalten des Zustands
+
+        if (isSaved) {
+            this.src = "../../assets/images/icons/bookmark-fill.svg"; // Gespeichert-Bild
+        } else {
+            this.src = "../../assets/images/icons/bookmark_unsaved.svg"; // Nicht gespeichert-Bild
+        }
+    });
