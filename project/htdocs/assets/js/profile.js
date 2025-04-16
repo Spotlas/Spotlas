12e398502f5e641c7dd34632ac579ee00c62dbf7
annar2703 <anna.reder2703ar@gmail.com>
2025-04-16 20:44:54 +0200

    profile seite fertig

--- project/htdocs/assets/js/profile.js
+++ project/htdocs/assets/js/profile.js
@@ -12,40 +12,40 @@ loadErstellteBilder();
 function loadErstellteBilder() {
   document.getElementById("bilder").innerHTML = `
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">3 ❤</p></div>
   </div>
     <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">23 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">4 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">1 ❤</p></div>
   </div>
     <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">456 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">3 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">4 ❤</p></div>
   </div>
     <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">4 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">5 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">6 ❤</p></div>
   </div>
     <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">1 ❤</p></div>
   </div>
   <div class="image-wrapper"><img class="images" src="../../assets/images/testPic/1.png" alt="test" />
-    <div class="overlay"><h2 class="header">Lake</h2><p class="like">❤</p></div>
+    <div class="overlay"><h2 class="header">Lake</h2><p class="like">0 ❤</p></div>
   </div>`;
 }
 
@@ -58,136 +58,28 @@ function loadFavouriten(clickedPTag) {
   showLine(clickedPTag);
   document.getElementById("bilder").innerHTML = `
     <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
     </div>
       <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
-      </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-      <div class="overlay">Overlay Text</div>
-    </div>
-      <div class="image-wrapper"><img class="images_fav" src="../../assets/images/testPic/2.png" alt="test" />
-        <div class="overlay">Overlay Text</div>
       </div>`;
 }
 
